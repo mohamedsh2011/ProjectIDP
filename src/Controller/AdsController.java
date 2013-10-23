3ec76206af2ae90d7183074bd20139458b1d8937
lliinnie <linyi1991@hotmail.com>
2013-10-23 19:28:48 +0800

    Update AdsController.java

--- src/Controller/AdsController.java
+++ src/Controller/AdsController.java
@@ -23,8 +23,39 @@ public class AdsController {
 		return toSend;
 	}
 	
+	public static ArrayList<Ads> retrieveAllAds(){
+		ArrayList<Ads> allAds = AdsManager.retrieveAllAds();
+		return allAds;
+	}
 	
+	public static ArrayList<Ads> retrieveMyAds(){
+		
+		ArrayList<Ads> toSend = new ArrayList<Ads>();
+		toSend = AdsManager.retrieveMyAds();
+		
+		return toSend;
+	}	
 	
+	public static ArrayList<Ads> addAd(String name){
+		System.out.println("AdsController:" + name);
+		ArrayList<Ads> toSend = new ArrayList<Ads>();
+		toSend = AdsManager.addAd(name);
+		System.out.println("AdsController:" + toSend.size());
+		
+		return toSend;
+	}	
 	
+	public static ArrayList<Ads> removeAd(String name){
+		
+		ArrayList<Ads> toSend = new ArrayList<Ads>();
+		toSend = AdsManager.removeAd(name);
+		
+		return toSend;
+	}	
+	
+	public static String isSaved(Ads a){
+		String value = AdsManager.isSaved(a);
+		return value;
+	}
 	
 }
